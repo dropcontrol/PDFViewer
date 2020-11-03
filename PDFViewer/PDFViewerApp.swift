616290ca0c517aa449554b10ff50ec0b096635cf
hiroshi yamato <yamato@gmail.com>
2020-11-03 19:08:41 +0900

    Initial Commit

---
+++ PDFViewer/PDFViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewerApp.swift
+//  PDFViewer
+//
+//  Created by hiroshi yamato on 2020/11/03.
+//
+
+import SwiftUI
+
+@main
+struct PDFViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
